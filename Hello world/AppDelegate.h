2b645d1c7db64e607e2ef65c8aafb130e1939c3f
Pratap <pratap@prataps-macbook-pro.local>
2016-01-01 22:20:57 +0530

    Initial Commit

---
+++ Hello world/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Hello world
+//
+//  Created by Pratap on 01/01/16.
+//  Copyright © 2016 Pratap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Hello world/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello world
+//
+//  Created by Pratap on 01/01/16.
+//  Copyright © 2016 Pratap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
